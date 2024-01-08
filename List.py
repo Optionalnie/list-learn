fac5567baf78dfd3e2b9df1a93016f0c5c04543a
Optionalnie <mikolaj.matenka@gmail.com>
2024-01-08 19:46:56 +0100

    First mein Commit

---
+++ List.py
@@ -0,0 +1,19 @@
+# spam = ['kot', 'Nasa z Nysy Kłodzkiej', 'Lambert w neglizu', 'PzkpfIV']
+# print(spam)
+# print('elo')
+# print(spam[0])
+
+# pythonizm = [['Koklusz','Maka krupczatka','Maslo','Saracen'],['Robert Kubica','Reka Roberta Kubicy','Roberto Carlos']]
+# print(pythonizm[1][2])
+
+wypiernicz = ['męstwo', 'heroizm', 'hedonizm', 'masochizm', 'miłosć']
+print(wypiernicz[:1])
+print(len(wypiernicz))
+wypiernicz[3] = 'zygmunt chajzer'
+print(wypiernicz)
+wypiernicz[3] = wypiernicz [1]
+print(wypiernicz)
+latweria = [1,2,3] + ['Alicja', 'Krętacz', 'Pogrzebacz']*3
+print(latweria)
+del latweria[3:]
+print(latweria)
